edf5add45e0683bb72c29d63f5945f0203e39aea
Maxoin <39875172+Maxoin@users.noreply.github.com>
2019-03-08 16:03:55 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -319,7 +319,7 @@ bot.on('message', message => { //Appartion MOwOnster
      var spawn = Math.floor(Math.random() * Math.floor(100))
      console.log("wala")
      if(spawn <= 8){
-       var kispawn =  Math.floor(Math.random() * Math.floor(20))
+       var kispawn =  Math.floor(Math.random() * Math.floor(22))
        console.log(kispawn)
          ideydb = db.get('mowo').filter({idey: kispawn}).find('idey').value()
          ideye = Object.values(ideydb)
