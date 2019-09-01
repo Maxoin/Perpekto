58cfb310003ade6f77f72af31d85c6a2b0f7d8da
Maxoin <39875172+Maxoin@users.noreply.github.com>
2019-09-01 17:09:56 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -408,7 +408,7 @@ bot.on('ready',() => {
     console.log('Bot Ready')
 })
 
-bot.login("process.env.token");
+bot.login(process.env.token);
 
 bot.on('message', message => { //help
   if(message.content === "!!help"){
