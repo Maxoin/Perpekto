b5ecaa58ac52f172fdb60e43f62d64c885e14d40
Maxoin <39875172+Maxoin@users.noreply.github.com>
2019-09-01 17:06:38 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -487,7 +487,7 @@ bot.on('message', message => { //Appartion MOwOnster
          .addField(`C'est un ${mowo[kispawn].nom} !`, 'Attrape le avec un "!!cat" !')
          .setImage(mowo[kispawn].image)
          .setColor("#351cc0")
-         bot.channels.get("617644715373887500").send(embedp)
+         bot.channels.get("612569111343530014").send(embedp)
          catnum = kispawn
          salon = message.channel.id
      }
